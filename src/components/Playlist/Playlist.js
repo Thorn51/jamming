c7d09d6c6c8b9ac9db8e12d9e57f95a4edf16bfa
Matt Thornberry <mtberry16@hotmail.com>
2021-09-17 09:02:56 -0600

    Add styles

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { TrackList } from '../TrackList/TrackList'
+import './Playlist.css'
 
 const PlayList = () => {
     return (
-        <div class="Playlist">
-            <input value="New Playlist"/>
-            <TrackList />
-            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+        <div className="Playlist">
+            <input defaultValue={'New Playlist'}/>
+            {/* <TrackList /> */}
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     )
 }
