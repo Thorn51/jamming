23fa4302fb8d2e6e1b239cdd61bfac7c710b1cdd
Matt Thornberry <mtberry16@hotmail.com>
2021-09-17 08:57:58 -0600

    Create funciton component and add JSX

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PlayList = () => {
+    return (
+        <div class="Playlist">
+            <input value="New Playlist"/>
+            {/* <!-- Add a TrackList component --> */}
+            <button class="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+    )
+}
+
+export default PlayList
