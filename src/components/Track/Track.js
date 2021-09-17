a31c5fd1ab4af87bbd0bc57cf1fa007b05b879b1
Matt Thornberry <mtberry16@hotmail.com>
2021-09-17 10:52:15 -0600

    Add function component

---
+++ src/components/Track/Track.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Track = () => {
+
+    renderAction = () => {
+        const action = isRemoval ? '-' : '+'
+        return <button className="Track-action">{action}</button>
+    }
+
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>Track Name</h3>
+                <p>Track Artist | Album</p>
+            </div>
+            <button className="Track-action">-- + or - will go here</button>
+        </div>
+    )
+}
+
+export default Track
