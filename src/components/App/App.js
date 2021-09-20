1883f556821cb9c5bef929f1506969c1ca4b00c1
Matt Thornberry <mtberry16@hotmail.com>
2021-09-20 12:08:07 -0600

    Setup useState hook for search results

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults'
@@ -5,13 +6,15 @@ import Playlist from '../Playlist/Playlist'
 import './App.css'
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([])
+  console.log(searchResults)
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults />
+          <SearchResults searchResults={searchResults} />
           <Playlist />
         </div>
       </div>
