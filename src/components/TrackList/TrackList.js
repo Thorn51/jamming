ef6c2e078a923019bb92e915c589a6c688b682fb
Matt Thornberry <mtberry16@hotmail.com>
2021-09-21 07:06:55 -0600

    Render list of tracks to playlist

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Track from '../Track/Track'
 import './TrackList.css'
 
-const TrackList = () => {
+const TrackList = (props) => {
     return (
         <div className="TrackList">
-            {/* <!-- You will add a map method that renders a set of Track components  --> */}
+            {props.tracks.map(track => {
+                return <Track key={track.id} track={track} onAdd={props.onAdd} />
+            })}
         </div>
     )
 }
