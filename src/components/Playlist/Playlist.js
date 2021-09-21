09641fe98cad786a7a6ee589f1516d1a917c60f1
Matt Thornberry <mtberry16@hotmail.com>
2021-09-21 07:04:46 -0600

    Render playlist

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -2,11 +2,11 @@ import React from 'react'
 import TrackList from '../TrackList/TrackList'
 import './Playlist.css'
 
-const Playlist = () => {
+const Playlist = (props) => {
     return (
         <div className="Playlist">
             <input defaultValue={'New Playlist'}/>
-            {/* <TrackList /> */}
+            {props.tracks && <TrackList tracks={props.tracks} />}
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     )
