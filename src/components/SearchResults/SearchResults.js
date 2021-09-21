951eea0c7343cbf66d040416c4d8c5fa238ce6db
Matt Thornberry <mtberry16@hotmail.com>
2021-09-21 07:05:48 -0600

    Pass method to add songs to playlist

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -6,7 +6,7 @@ const SearchResult = (props) => {
     return (
         <div className="SearchResults">
             <h2>Results</h2>
-            <TrackList tracks={props.searchResults} />
+            <TrackList tracks={props.searchResults} onAdd={props.onAdd} isRemoval={false} />
         </div>
     )
 }
