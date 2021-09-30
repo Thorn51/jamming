7c30b376a069caca4871d0065576c1a1c1c952d9
Matt Thornberry <mtberry16@hotmail.com>
2021-09-30 07:21:02 -0600

    Refactor to dropdown menu

--- src/components/PlaylistCollection/PlaylistCollection.js
+++ src/components/PlaylistCollection/PlaylistCollection.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import PlaylistItem from '../PlaylistItem'
 import './PlaylistCollection.css'
 
-const PlaylistCollection = (props) => {
+const PlaylistCollection = ({playlists, onSelect}) => {
+    
+    const handleSelect = (e) => {
+        onSelect(e.target.value)
+    }
+
     return (
-        <div className="PlaylistCollection">
-            <PlaylistItem />
+        <div className="Playlist-collection">
+            <select name="playlists" id="playlists" onChange={handleSelect}>
+                <option value="label">Select a playlist...</option>
+                {playlists.map(list => <option key={list.id}  value={list.id}>{list.name}</option>)}
+                <option value="newList">Create a new playlist</option>
+            </select>
         </div>
     )
 }
+
+export default PlaylistCollection
